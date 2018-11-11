d631224d5b64e006236390b7ae64d339ff05e70f
[hnakae] <[nakae27@gmail.com]>
2018-11-10 17:54:16 -0800

    tile color

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-// $("div.row").on("click", "div.box", function(){
-//     $(this).toggleClass("clicked");
-// });
-
 $(document).ready(function () {
 
     var move = 1;
     var play = true;
 
-    function resetBoard(){
-        location.reload();
-    }
-
-
-
     $("div.inner").click(function () {
-        if ($(this).text() != "" && play) {
+        if ($(this).text() == "" && play) {
             if ((move % 2) == 1) {
                 $(this).toggleClass("black"); 
             }
@@ -23,11 +13,10 @@ $(document).ready(function () {
             }
             move++;
         }
-        
     });
 
    $(".reset").click(function(){
-        resetBoard();
+       location.reload();
    });
 
 });
@@ -35,26 +24,3 @@ $(document).ready(function () {
 
 
 
-// while(counter < 10){
-//     if (counter % 2 == 0) {
-//         $("div.box").click(function () {
-//             $(this).toggleClass("black");
-//             counter++;
-//         });
-//     } else {
-//         $("div.box").click(function () {
-//             $(this).toggleClass("white");
-//             counter++;
-//         });
-//     }
-// }
-
-
-
-
-
-// if ($(this).css("background") != "black") {
-//     $(this).clicked();
-// } else {
-//     $(this).clickedAgain();
-// }
